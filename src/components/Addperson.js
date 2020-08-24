82e5234788152e607216ace7a7c56f27c7bad644
TeukkaK <48186687+TeukkaK@users.noreply.github.com>
2020-08-24 10:48:45 +0300

    Add files via upload

---
+++ src/components/Addperson.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Addperson = (props) => {
+    return(
+        <form onSubmit={props.addPerson}>
+           <div>
+              name: <input value={props.newName} onChange={props.nameChange}/>
+              number: <input value={props.newNumber} onChange={props.numberChange}/>
+            </div>
+            <div>
+               <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+export default Addperson;
